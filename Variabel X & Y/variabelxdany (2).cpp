b6bf4c0a5c9d9d6b88ebfe3dc918e5986e959ff8
DELLASA <113181918+DELLASA@users.noreply.github.com>
2023-01-11 23:35:54 +0700

    Add files via upload

---
+++ Variabel X & Y/variabelxdany (2).cpp
@@ -0,0 +1,24 @@
+//judul : Menjumlahkan 2 angka variabel x dan y
+//Della Sabrina
+//A11.2022.14019
+//17 September 2022
+
+#include <iostream>
+
+
+using namespace std;
+
+//kamus
+int main()
+{
+    int x;
+    int y;
+    float hasilpenjumlahanxdany;
+
+//diskripsi
+    x = 7;
+    y = 3;
+    hasilpenjumlahanxdany = (x + y);
+    cout << hasilpenjumlahanxdany << endl;
+    return 0;
+}
