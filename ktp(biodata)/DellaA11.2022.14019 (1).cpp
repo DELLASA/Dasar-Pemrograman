eb25669f17aabbbfbce1e964334451bed399f2b1
DELLASA <113181918+DELLASA@users.noreply.github.com>
2023-01-11 23:25:58 +0700

    Add files via upload

---
+++ ktp(biodata)/DellaA11.2022.14019 (1).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    cout << "     PROVINSI JAWA TENGAH" << endl;
+    cout << "     KABUPATEN JAWA TENGAH" << endl;
+    cout << "  NIM               : A11.2022.14019" << endl;
+    cout << "  Nama              : DELLA SABRINA" << endl;
+    cout << "" << endl;
+    cout << "  Tempat/Tgl lahir  : GROBOGAN, 20-12-2004" <<endl;
+    cout << "  Jenis Kelamin     : PEREMPUAN" << endl;
+    cout << "  Alamat            : PERUM GRIYA PRAJA INDAH" << endl;
+    cout << "      RT/RW         : 04/023" << endl;
+    cout << "      Kel/Desa      : KURIPAN" << endl;
+    cout << "      Kecamatan     : Purwodadi" << endl;
+    cout << "  Agama             : Islam" << endl;
+    cout << "  Status Perkawinan : Belum Kawin" << endl;
+    cout << "  Pekerjaan         : Mahasiswa" << endl;
+    cout << "  Kewarganegaraan   : WNI" << endl;
+    cout << "  Berlaku Hingga    : Seumur Hidup";
+
+}
